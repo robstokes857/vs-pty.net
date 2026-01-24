b20cd1f175fe62f685568cb82fcea6379f29a682
Robert Stokes <robstokes857@gmail.com>
2026-01-24 02:18:52 -0600

    stash

--- src/Pty.Net/EzTerminal.cs
+++ src/Pty.Net/EzTerminal.cs
@@ -101,11 +101,14 @@ public class EzTerminal : IEzTerminal
                 while (!token.IsCancellationRequested)
                 {
                     var key = Console.ReadKey(intercept: true);
-                    var input = key.KeyChar.ToString();
-                    _userOutputHandler(input);
-                    var bytes = System.Text.Encoding.UTF8.GetBytes(input);
-                    await _pty.WriterStream.WriteAsync(bytes, token);
-                    await _pty.WriterStream.FlushAsync(token);
+                    var input = TranslateKey(key);
+                    if (!string.IsNullOrEmpty(input))
+                    {
+                        _userOutputHandler(input);
+                        var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+                        await _pty.WriterStream.WriteAsync(bytes, token);
+                        await _pty.WriterStream.FlushAsync(token);
+                    }
                 }
             }
             catch (OperationCanceledException) { }
@@ -128,5 +131,40 @@ public class EzTerminal : IEzTerminal
     public void Dispose()
     {
         _pty?.Dispose();
+    }
+
+    private static string TranslateKey(ConsoleKeyInfo key)
+    {
+        // Handle special keys that need ANSI escape sequences
+        return key.Key switch
+        {
+            ConsoleKey.UpArrow => "\x1B[A",
+            ConsoleKey.DownArrow => "\x1B[B",
+            ConsoleKey.RightArrow => "\x1B[C",
+            ConsoleKey.LeftArrow => "\x1B[D",
+            ConsoleKey.Home => "\x1B[H",
+            ConsoleKey.End => "\x1B[F",
+            ConsoleKey.Delete => "\x1B[3~",
+            ConsoleKey.Insert => "\x1B[2~",
+            ConsoleKey.PageUp => "\x1B[5~",
+            ConsoleKey.PageDown => "\x1B[6~",
+            ConsoleKey.F1 => "\x1BOP",
+            ConsoleKey.F2 => "\x1BOQ",
+            ConsoleKey.F3 => "\x1BOR",
+            ConsoleKey.F4 => "\x1BOS",
+            ConsoleKey.F5 => "\x1B[15~",
+            ConsoleKey.F6 => "\x1B[17~",
+            ConsoleKey.F7 => "\x1B[18~",
+            ConsoleKey.F8 => "\x1B[19~",
+            ConsoleKey.F9 => "\x1B[20~",
+            ConsoleKey.F10 => "\x1B[21~",
+            ConsoleKey.F11 => "\x1B[23~",
+            ConsoleKey.F12 => "\x1B[24~",
+            ConsoleKey.Tab => "\t",
+            ConsoleKey.Enter => "\r",
+            ConsoleKey.Backspace => "\x7F",
+            ConsoleKey.Escape => "\x1B",
+            _ => key.KeyChar == '\0' ? string.Empty : key.KeyChar.ToString()
+        };
     }
 }
